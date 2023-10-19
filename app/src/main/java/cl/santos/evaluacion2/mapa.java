b2feb71ab3be7ef494ad8fab6c398298ef24b9bb
Joan000111 <aracely.a.cifuentes.v@gmail.com>
2023-10-19 18:27:53 -0300

    e

--- app/src/main/java/cl/santos/evaluacion2/mapa.java
+++ app/src/main/java/cl/santos/evaluacion2/mapa.java
@@ -3,28 +3,26 @@ package cl.santos.evaluacion2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest.permission;
-import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import android.Manifest;
-
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PointOfInterest;
 
 //al momento de implemetar y tal, notar que hay que tener lo demás agregado en el gradle y
 //el manifest, no dejaba ingresar el modulo sin tener antes esos trozos de código
@@ -33,6 +31,12 @@ import com.google.android.gms.maps.model.PointOfInterest;
 public class mapa extends AppCompatActivity implements OnMapReadyCallback {
 
     ImageButton volvere;
+
+    private Marker marcadore;
+    double lat = 0.0;
+    double lon = 0.0;
+    //GoogleMap mMap = googleMap;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,18 +80,49 @@ public class mapa extends AppCompatActivity implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(punto1)
                 .title("Primer Marcador")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(punto1));
 
         LatLng punto2 = new LatLng(lon2, lat2);
         mMap.addMarker(new MarkerOptions().position(punto2)
                 .title("Segundo Marcador")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(punto2));
 
         LatLng punto3 = new LatLng(lon3, lat3);
         mMap.addMarker(new MarkerOptions().position(punto3)
                 .title("Tercer Marcador")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(punto3));
+
+        miubicacion();
+    }
+
+    private void agregarMarcador(double lat, double lon) {
+        LatLng coordenadas = new LatLng(lat, lon);
+        CameraUpdate miubicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
+        mMap.addMarker(new MarkerOptions().position(coordenadas)
+                .title("Posicion Actual")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.animateCamera(miubicacion);
+    }
+
+    private void actualizarubicacion(Location location) {
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+        agregarMarcador(lat, lon);
+    }
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            actualizarubicacion(location);
+        }
+    };
+
+    private void miubicacion() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0,locListener);
+        actualizarubicacion(location);
     }
 }
